b091746289b75882dcfe820e7d00963f075924a2
hubgitpoonam <poonamkumarirt428@gmail.com>
2025-05-29 18:06:22 +0530

    Poonam29may

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import appReducer from '../features/app/appSlice';
+import contactReducer from '../features/contact/contactSlice';
+import itineraryReducer from '../features/itinerary/itinerarySlice';
 
 export const store = configureStore({
   reducer: {
     app: appReducer,
+    contact: contactReducer,
+    itinerary: itineraryReducer,
   },
 });
